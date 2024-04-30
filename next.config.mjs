e31fe65b6bbfe591a10abfdc52c148b96f73320c
Vipul Shetty <135506557+vipulshetty@users.noreply.github.com>
2024-04-30 13:16:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,4 @@ const nextConfig = {
     },
 };
 
-=======
-   eslint: {
-        ignoreDuringBuilds: true,
-    },
-};
->>>>>>> 115c6d33cc9ee6313e7b57b32054bef9e736beb1
-
 export default nextConfig;
